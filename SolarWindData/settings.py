84b369aa7fbd82f53de90425bfb58c52dbe8d40b
Maya Taborga <valentina.taborga@usach.cl>
2024-11-18 00:23:32 -0300

    sql???

--- SolarWindData/settings.py
+++ SolarWindData/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'SolarWindData.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/SolarWindData',
         conn_max_age=600
     )
 }
