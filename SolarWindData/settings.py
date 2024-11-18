d19ba0c69feb1d52cb5b645472ab25d14039a722
Maya Taborga <valentina.taborga@usach.cl>
2024-11-18 00:55:14 -0300

    a

--- SolarWindData/settings.py
+++ SolarWindData/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATICFILES_DIRS =  os.path.join(BASE_DIR, 'staticfiles')  
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
